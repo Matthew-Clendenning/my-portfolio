565e5564e706382e638e85dcdb4168f4f70bbf03
Matthew Clendenning <matthewclendenning@outlook.com>
2023-05-31 20:54:59 -0500

    navbar commit

--- app/page.js
+++ app/page.js
@@ -1,8 +1,9 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Navbar from '../components/navbar'
 
 export default function Home() {
   return (
-    <h1>Hello World!</h1>
+    <Navbar />
   )
 }
