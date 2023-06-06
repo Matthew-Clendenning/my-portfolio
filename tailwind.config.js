947c99c19399041c498eeb15e6b1a22af9692212
Matthew Clendenning <matthewclendenning@outlook.com>
2023-06-05 19:50:16 -0500

    projects commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,29 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      dropShadow: {
-        '3xl': '0 35px 35px rgba(0, 0, 0, 0.25)',
-        '4xl': [
-            '0 0px 35px rgba(0, 0, 0, 0.35)',
-            '0 45px 65px rgba(0, 0, 0, 0.05)'
-        ]
-      },
-      animation: {
-        fadeIn: "fadeIn 1.5s ease-in-out forwards"
-      },
-      keyframes: {
-        fadeIn: {
-          "0%": { opacity: 0 },
-          "100": { opacity: 1 }
-        }
-      }
+
+export const content = [
+  "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  "./components/**/*.{js,ts,jsx,tsx,mdx}",
+];
+export const theme = {
+  extend: {
+    dropShadow: {
+      '3xl': '0 35px 35px rgba(0, 0, 0, 0.25)',
+      '4xl': [
+        '0 0px 35px rgba(0, 0, 0, 0.35)',
+        '0 45px 65px rgba(0, 0, 0, 0.05)'
+      ]
     },
+    animation: {
+      fadeIn: "fadeIn 1.5s ease-in-out forwards"
+    },
+    keyframes: {
+      fadeIn: {
+        "0%": { opacity: 0 },
+        "100": { opacity: 1 }
+      }
+    }
   },
-  plugins: [],
-}
+};
+export const plugins = [];
 
