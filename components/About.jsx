599354798005cdcb904f4abaed638e723e685ba3
Matthew Clendenning <matthewclendenning@outlook.com>
2023-06-17 22:42:49 -0500

    about commit

--- components/About.jsx
+++ components/About.jsx
@@ -29,9 +29,7 @@ const About = () => {
                 <div className='flex justify-center rounded-lg drop-shadow-4xl sm:hover:scale-105 sm:ease-in sm:duration-300'>
                     <Image 
                         src={aboutPic} 
-                        priority='true' 
-                        width='612'
-                        height='612' 
+                        width={250}
                         alt="person coding"
                     />
                 </div>
--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'https://matt-dev.vercel.app/',
+            }
+        ],
+    },
+}
 
 module.exports = nextConfig
